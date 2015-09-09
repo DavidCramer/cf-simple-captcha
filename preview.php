6d083e37611514ff29c093a1c4fc718fbc122980
David Cramer <david@digilab.co.za>
2015-09-09 08:17:58 +0200

    no placeholder

--- preview.php
+++ preview.php
@@ -1,7 +1,7 @@
 <div class="preview-caldera-config-group">
 	{{#unless hide_label}}<lable class="control-label">{{label}}{{#if required}} <span style="color:#ff0000;">*</span>{{/if}}</lable>{{/unless}}
 	<div class="preview-caldera-config-field">
-		2 + 3 = <input style="max-width: 50px;" {{#if hide_label}}placeholder="{{label}}"{{else}}placeholder="{{config/placeholder}}"{{/if}} type="text" class="preview-field-config" value="{{config/default}}">
+		2 + 3 = <input style="max-width: 50px;" type="text" class="preview-field-config" value="{{config/default}}">
 		<span class="help-block">{{caption}}</span>
 	</div>
 </div>

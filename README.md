e2f9b009d9b47928ab610eb85099d355af5e987a
David Cramer <david@digilab.co.za>
2015-09-09 08:25:26 +0200

    readme example

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cf-simple-captcha
-A Simple math based captcha field
+A Simple math based captcha field for Caldera Forms
+<img src="https://www.dropbox.com/s/g62gym16m6sbfhw/simple-captcha.gif?dl=1" />
+
--- field.php
+++ field.php
@@ -12,7 +12,7 @@ $third = rand(1, $first - 1);
 	<?php echo $field_label; ?>
 	<?php echo $field_before; ?>
 		<input <?php echo $field_placeholder; ?> type="hidden" name="cfscf[<?php echo $field['ID'] . $second . $first . $third; ?>]" value="<?php echo $field['ID']; ?>">
-		<?php echo $first . ' ' . $opps[ $second ] . ' ' . $third; ?> = <input style="max-width: 50px; display: inline;" type="text" data-field="<?php echo $field_base_id; ?>" class="<?php echo $field_class; ?>" id="<?php echo $field_id; ?>" name="<?php echo $field_name; ?>" value="<?php echo esc_attr( $field_value ); ?>" required>
+		<?php echo $first . ' ' . $opps[ $second ] . ' ' . $third; ?> = <input autocomplete="off" style="max-width: 50px; display: inline;" type="text" data-field="<?php echo $field_base_id; ?>" class="<?php echo $field_class; ?>" id="<?php echo $field_id; ?>" name="<?php echo $field_name; ?>" value="<?php echo esc_attr( $field_value ); ?>" required>
 		<?php echo $field_caption; ?>
 	<?php echo $field_after; ?>
 <?php echo $wrapper_after; ?>
